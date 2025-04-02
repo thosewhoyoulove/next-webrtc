b2b6b925c78f726f7c77b979c8c3752dce3ce529
thosewhoyoulove <2406001659@qq.com>
2025-04-02 22:28:28 +0800

    style: add tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
-    darkMode: "class",
+export default config = {
+    content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {},
     },
